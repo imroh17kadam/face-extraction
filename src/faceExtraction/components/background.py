72d5f4655d8e8f416c2b7f134282e87edc35c298
imroh17kadam <kadam17rohit@gmail.com>
2024-07-14 13:14:34 +0530

    Resized the image to square

--- src/faceExtraction/components/background.py
+++ src/faceExtraction/components/background.py
@@ -2,6 +2,7 @@ from rembg import remove
 import cv2
 from PIL import Image
 import io
+import numpy as np
 
 
 class BackgroundRemover:
@@ -14,8 +15,10 @@ class BackgroundRemover:
         # Load the image
         input_image = cv2.imread(self.input_path)
         
+        resized_image = self.resize_to_square(input_image)
+
         # Remove the background
-        output_image = remove(input_image)
+        output_image = remove(resized_image)
 
         face_pil = Image.fromarray(output_image)
         datas = face_pil.getdata()
@@ -39,3 +42,18 @@ class BackgroundRemover:
         print(f"Image saved to {self.output_path}")
 
         return output_image, output_buffer
+    
+    def resize_to_square(self, image):
+        # Get dimensions of the image
+        h, w = image.shape[:2]
+
+        # Determine the size of the square
+        size = max(h, w)
+
+        # Create a square background
+        square_bg = np.zeros((size, size, 3), dtype=np.uint8)
+        start_h = (size - h) // 2
+        start_w = (size - w) // 2
+        square_bg[start_h:start_h + h, start_w:start_w + w] = image
+
+        return square_bg
